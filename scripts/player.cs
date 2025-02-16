b5f0a27d0cbcccac8c9d24cef3355f824bc83d38
eric <eric@vibeground.com>
2025-02-16 22:43:57 +0200

    main mechanics

--- scripts/player.cs
+++ scripts/player.cs
@@ -1,30 +1,56 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public partial class player : CharacterBody2D
 {
 	public const float Speed = 100.0f;
 	public const float JumpVelocity = -300.0f;
+	private Queue<Vector2> steps = new Queue<Vector2>();
+	private bool frozen = true;
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 velocity = Velocity;
-
-		// Add the gravity.
+		AnimatedSprite2D me = GetNode<AnimatedSprite2D>("me");
 		if (!IsOnFloor())
 		{
-			velocity += GetGravity() * (float)delta;
+			Velocity += GetGravity() * (float)delta;
 		}
+		if (frozen)
+		{
+			Modulate = new Color(1, 0, 0);
+			me.SpeedScale = 0.2f;
+			Vector2 direction = Input.GetVector("left", "right", "up", "down");
+			if (steps.Count > 0 || direction != Vector2.Zero)
+			{
+				steps.Enqueue(direction);
+			}
+		}
+		else
+		{
+			Modulate = new Color(1, 1, 1);
+			me.SpeedScale = 1.0f;
+			play(delta, steps.Dequeue());
+			if (steps.Count == 0)
+			{
+				frozen = true;
+			}
+		}
+		if (Input.IsActionJustReleased("start"))
+		{
+			frozen = false;
+		}
+		MoveAndSlide();
+	}
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("jump") && IsOnFloor())
+	public void play(double delta, Vector2 direction)
+	{
+		Vector2 velocity = Velocity;
+		if (direction.Y < 0 && IsOnFloor())
 		{
 			velocity.Y = JumpVelocity;
 		}
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("left", "right", "up", "down");
 		if (direction != Vector2.Zero)
 		{
 			velocity.X = direction.X * Speed;
@@ -33,8 +59,6 @@ public partial class player : CharacterBody2D
 		{
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 		}
-
 		Velocity = velocity;
-		MoveAndSlide();
 	}
 }
