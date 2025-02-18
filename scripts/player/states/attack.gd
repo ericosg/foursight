ab1fa2b828fa04e50a058b238ec733433fca4348
eric <eric@loadx.org>
2025-02-18 21:25:20 +0200

    combo attacks

--- scripts/player/states/attack.gd
+++ scripts/player/states/attack.gd
@@ -3,18 +3,21 @@ extends State
 @export var sheath_state: State
 @export var next_state: State
 
+var can_next := false 
+
 func enter() -> void:
+	can_next = false
 	parent.can_move = false
 	super()
 
 func process_input(event: InputEvent) -> State:
 	if next_state && Input.is_action_just_pressed('fast'):
-		return next_state
+		can_next = true
 	return null
 
 func process_frame(delta: float) -> State:
 	if parent.can_move:
-		return sheath_state
+		return next_state if can_next else sheath_state
 	return null
 
 func _on_animations_animation_finished() -> void:
