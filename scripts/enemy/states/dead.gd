7e55ac9897544b5dd561fb6ec599d018d731d94d
eric <eric@loadx.org>
2025-02-22 19:00:59 +0200

    get to stage 2

--- scripts/enemy/states/dead.gd
+++ scripts/enemy/states/dead.gd
@@ -4,6 +4,7 @@ func enter() -> void:
 	super()
 	parent.collision.disabled = true
 	parent.hit_area.disabled = true
+	parent.animations.speed_scale = Settings.NormalSpeedScale
 
 func animation_finished() -> State:
 	parent.queue_free()
--- scripts/stage_1.gd
+++ scripts/stage_1.gd
@@ -3,3 +3,8 @@ extends Node2D
 func _ready():
 	Global.Freeze()
 	Global.CutScene = false
+
+func _on_child_order_changed() -> void:
+	var enemy_count := get_children().filter(func(n): return n is AnimatableBody2D).size()
+	if enemy_count == 0 and get_tree():
+		get_tree().change_scene_to_file("res://scenes/stage2.tscn")
