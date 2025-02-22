f259a7ee69294f28fe2eca0e469000426a1fab93
eric <eric@loadx.org>
2025-02-22 19:41:01 +0200

    new start button

--- scripts/buttons.gd
+++ scripts/buttons.gd
@@ -1,31 +1,39 @@
 extends Node2D
 
-@onready var buttons := $"."
-
 const BUTTONS_FIT_ON_SCREEN = 31
 	
 func _unhandled_input(event: InputEvent) -> void:
 	if not Global.CutScene:
 		if event.is_action_pressed('up'):
 			add_button(0)
+			print('action = ', event.as_text())
 		if event.is_action_pressed('right'):
 			add_button(1)
+			print('action = ', event.as_text())
 		if event.is_action_pressed('down'):
 			add_button(2)
+			print('action = ', event.as_text())
 		if event.is_action_pressed('left'):
 			add_button(3)
+			print('action = ', event.as_text())
 		if event.is_action_pressed('fast'):
 			add_button(4)
+			print('action = ', event.as_text())
 		if event.is_action_pressed('hard'):
 			add_button(5)
+			print('action = ', event.as_text())
+		if event.is_action_pressed('start'):
+			add_button(6)
+			print('action = ', event.as_text())
+			
 		
 func add_button(frame: int):
-	var button := buttons.get_child(0)
+	var button := get_child(0)
 	var next := button.duplicate()
 	next.frame = frame
-	buttons.position.x -= 16
-	next.position.x = abs(buttons.position.x)
+	position.x -= 16
+	next.position.x = abs(position.x)
 	next.visible = true
 	add_child(next)
-	if buttons.get_child_count() > BUTTONS_FIT_ON_SCREEN:
+	if get_child_count() > BUTTONS_FIT_ON_SCREEN:
 		button.queue_free()
